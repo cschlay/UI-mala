05fc1cef00326260172f0f91350b1a18e2ef4997
chi-hao.lay@outlook.com <development@cschlay.com>
2021-02-13 16:02:20 +0200

    Add sign in button

--- components/Menu.js
+++ components/Menu.js
@@ -1,15 +1,13 @@
+import { Button } from "./Button";
 import { Logo } from "./Logo";
 import styles from "./Menu.module.scss";
-import Link from "next/link";
 
 const Menu = () => {
   return (
     <div className={styles.Menu}>
       <Logo />
       <nav>
-        <Link href="/login">
-          <a>Login</a>
-        </Link>
+        <Button href="/login">Sign In</Button>
       </nav>
     </div>
   );
